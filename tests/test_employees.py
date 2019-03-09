62b8e740dca8354160cbfa2c24323953f7e46ad1
Tre <tre@tre.one>
2019-03-09 21:07:34 +0300

    Добавленны методы добавления, редактирования и удаления сотрудников.

--- tests/test_employees.py
+++ tests/test_employees.py
@@ -11,17 +11,33 @@ class ClassTestEmployees(unittest.TestCase):
         self.assertEqual(self.employees.num(), 0)
 
     def test_adding_employees(self):
-        self.assertEqual(self.employees.num(), 0)
         self.employees.add_empty_employee()
         self.assertEqual(self.employees.num(), 1)
 
     def test_getting_employees(self):
-        self.assertEqual(self.employees.num(), 0)
         self.employees.add_empty_employee()
-        self.assertIsNone(self.employees.get_employee(1))
         employee = self.employees.get_employee(0)
-        self.assertEqual(employee.fields['patronymic'], '')
-        self.assertIsInstance(employee.fields['hazard_factors'], list)
+        with self.assertRaises(KeyError):
+            employee.value('bla-bla-bla')
+        self.assertEqual(employee.value('patronymic'), '')
+        self.assertIsInstance(employee.value('hazard_factors'), list)
+
+    def test_edit_employee(self):
+        self.employees.add_empty_employee()
+        hazards = ['1', '2', '3']
+        employee = self.employees.get_employee(0)
+        employee.set_value('family_name', 'Иванов')
+        self.assertEqual(employee.value('family_name'), 'Иванов')
+        employee.set_value('hazard_types', hazards)
+        hazards.append('4')
+        self.assertEqual(len(employee.value('hazard_types')), 3)
+
+    def remove_employee(self):
+        self.employees.add_empty_employee()
+        self.employees.add_empty_employee()
+        self.assertEqual(self.employees.num(), 2)
+        self.employees.rem_employee(1)
+        self.assertEqual(self.employees.num(), 1)
 
 
 if __name__ == '__main__':
--- utility/employees.py
+++ utility/employees.py
@@ -6,24 +6,32 @@ class Employees:
         self.list_of_employees = dict()
         self.max_id = -1
 
-    def num(self):
+    def num(self) -> int:
         return len(self.list_of_employees)
 
     def get_new_id(self):
         self.max_id += 1
         return self.max_id
 
-    def add_empty_employee(self):
-        self.list_of_employees[self.get_new_id()] = Employee()
+    def get_employee(self, emp_id: int):
+        return self.list_of_employees[emp_id]
+
+    def rem_employee(self, emp_id: int):
+        return self.list_of_employees.pop(emp_id)
 
     def get_employees(self):
         return self.list_of_employees
 
-    def get_employee(self, emp_id: int):
-        try:
-            return self.list_of_employees[emp_id]
-        except KeyError:
-            return None
+    def add_empty_employee(self) -> int:
+        new_id = self.get_new_id()
+        self.list_of_employees[new_id] = Employee()
+        return new_id
+
+    def add_employee(self, employee: dict):
+        new_id = self.add_empty_employee()
+        new_employee = self.get_employee(new_id)
+        for k, v in employee:
+            new_employee.set_value(k, v)
 
 
 class Employee:
@@ -34,3 +42,15 @@ class Employee:
                 self.fields[field_name] = list()
             else:
                 self.fields[field_name] = ''
+
+    def set_value(self, field_name, value):
+        if field_name in self.fields:
+            if field_name in ('hazard_types', 'hazard_factors'):
+                self.fields[field_name] = value.copy()
+            else:
+                self.fields[field_name] = value
+        else:
+            raise KeyError
+
+    def value(self, field_name):
+        return self.fields[field_name]
